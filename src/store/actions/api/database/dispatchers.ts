04233771742fbe1fd407b82b9950bc0f45eecf9b
Arvind S <arvind0598@gmail.com>
2020-03-22 18:50:37 +0530

    added actions for api/database

---
+++ src/store/actions/api/database/dispatchers.ts
@@ -0,0 +1,40 @@
+import {
+  API_DATABASE_FETCH_START,
+  API_DATABASE_FETCH_SUCCESS,
+  API_DATABASE_FETCH_ERROR,
+  API_DATABASE_CHOOSE,
+} from './types';
+
+type ApiDatabaseFetchStartAction = {
+  type: typeof API_DATABASE_FETCH_START;
+};
+export const apiDatabaseFetchStart = (): ApiDatabaseFetchStartAction => ({
+  type: API_DATABASE_FETCH_START,
+});
+
+type ApiDatabaseFetchSuccessAction = {
+  type: typeof API_DATABASE_FETCH_SUCCESS;
+  data: string[];
+};
+export const apiDatabaseFetchSuccess = (data: string[]): ApiDatabaseFetchSuccessAction => ({
+  type: API_DATABASE_FETCH_SUCCESS,
+  data,
+});
+
+type ApiDatabaseFetchErrorAction = {
+  type: typeof API_DATABASE_FETCH_ERROR;
+  error: string;
+};
+export const apiDatabaseFetchError = (error: string): ApiDatabaseFetchErrorAction => ({
+  type: API_DATABASE_FETCH_ERROR,
+  error,
+});
+
+type ApiDatabaseChooseAction = {
+  type: typeof API_DATABASE_CHOOSE;
+  name: string;
+};
+export const apiDatabaseChoose = (name: string): ApiDatabaseChooseAction => ({
+  type: API_DATABASE_CHOOSE,
+  name,
+});
