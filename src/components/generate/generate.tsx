16e15b7f11ceddec151fa9084112018afe06b362
Arvind S <arvind0598@gmail.com>
2020-04-12 18:47:18 +0530

    generate button is disabled when there are no workspace entities

--- src/components/generate/generate.tsx
+++ src/components/generate/generate.tsx
@@ -3,15 +3,22 @@ import { Button } from 'grommet';
 import { LinkNext } from 'grommet-icons';
 import { connect } from 'react-redux';
 import { modalMainOpen } from '../../store/modal/dispatchers';
+import { StoreType } from '../../models/app/store';
+import { selectWorkspaceEntityNames } from '../../selectors/entities';
+
+type StoreProps = {
+  hasWorkspaceEntities: boolean;
+};
 
 type DispatchProps = {
   clickHandler: Function;
 };
 
-type Props = DispatchProps;
+type Props = StoreProps & DispatchProps;
 
 const GenerateButtonComponent = ({
   clickHandler,
+  hasWorkspaceEntities,
 }: Props): ReactElement => (
   <Button
     primary
@@ -19,14 +26,19 @@ const GenerateButtonComponent = ({
     size="large"
     gap="large"
     icon={<LinkNext />}
+    disabled={!hasWorkspaceEntities}
     onClick={(): void => clickHandler()}
   />
 );
+
+const mapStateToProps = (state: StoreType): StoreProps => ({
+  hasWorkspaceEntities: selectWorkspaceEntityNames(state).length > 0,
+});
 
 const mapDispatchToProps = (dispatch: Function): DispatchProps => ({
   clickHandler: (): void => dispatch(modalMainOpen()),
 });
 
-const GenerateButton = connect(null, mapDispatchToProps)(GenerateButtonComponent);
+const GenerateButton = connect(mapStateToProps, mapDispatchToProps)(GenerateButtonComponent);
 
 export default GenerateButton;
